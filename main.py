98521fc329562082509b9c83d119c214289855f5
drmehmetismail <125666501+drmehmetismail@users.noreply.github.com>
2024-02-25 22:03:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Main function for the Fast GI calculator
 from pgn_evaluation_fast_analyzer import main
-from stockfish_pgn_annotator import main
+from stockfish_pgn_annotator import main_stockfish
 import time
 
 start_time = time.time()
@@ -10,11 +10,11 @@ games_annotated = True
 
 if not games_annotated:
     # Set the dir paths for the PGN files and the output directory
-    input_dir_path = '...'
-    output_directory = '...'
+    input_dir_path = ''
+    output_directory = ''
     # set the path to the Stockfish executable
     # e.g.: 'C:\...\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe'
-    stockfish_path = '...'
+    stockfish_path = ''
     # Set the depth for the Stockfish analysis
     DEPTH = 20
     # Call the main function to annotate the games
@@ -23,11 +23,12 @@ if not games_annotated:
 # Set the input and output directories for the Fast GI calculator
 input_pgn_dir = '...'
 # Change the output directory if needed
-# output_json_dir = input_pgn_dir
-output_json_dir = '...'
-# Input win, draw, and loss values. Standard FIDE: 1,0.5,0. Norway Chess: 3,1.25,0 (normalized  by 1/3).
-wdl_values = [1, 0.5, 0]
-main(input_pgn_dir, output_json_dir, wdl_values)
+output_json_dir = input_pgn_dir
+# Set whether the GI score should be weighted by opponent's Elo
+weighted = False
+# Input win, draw, and loss values. Standard FIDE: [1,0.5,0]. Norway Chess: 3,1.25,0 (normalized  by 1/3).
+wdl_values = [1,0.5,0]
+main(input_pgn_dir, output_json_dir, wdl_values, weighted)
 
 end_time = time.time()
 print("Script finished in {:.2f} minutes".format((end_time - start_time) / 60.0))
