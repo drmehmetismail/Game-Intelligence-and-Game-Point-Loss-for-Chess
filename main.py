ce26668ebe3485dc1e086d6ddfe3b461fff51f46
drmehmetismail <125666501+drmehmetismail@users.noreply.github.com>
2024-02-25 20:42:55 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+# Main function for the Fast GI calculator
+from pgn_evaluation_fast_analyzer import main
+from stockfish_pgn_annotator import main
+import time
+
+start_time = time.time()
+
+# If the games are annotated with Stockfish, set this to True
+games_annotated = True
+
+if not games_annotated:
+    # Set the dir paths for the PGN files and the output directory
+    input_dir_path = '...'
+    output_directory = '...'
+    # set the path to the Stockfish executable
+    # e.g.: 'C:\...\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe'
+    stockfish_path = '...'
+    # Set the depth for the Stockfish analysis
+    DEPTH = 20
+    # Call the main function to annotate the games
+    main(input_dir_path, output_directory, stockfish_path, DEPTH)
+
+# Set the input and output directories for the Fast GI calculator
+input_pgn_dir = '...'
+# Change the output directory if needed
+# output_json_dir = input_pgn_dir
+output_json_dir = '...'
+# Input win, draw, and loss values. Standard FIDE: 1,0.5,0. Norway Chess: 3,1.25,0 (normalized  by 1/3).
+wdl_values = [1, 0.5, 0]
+main(input_pgn_dir, output_json_dir, wdl_values)
+
+end_time = time.time()
+print("Script finished in {:.2f} minutes".format((end_time - start_time) / 60.0))
